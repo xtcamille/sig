ea408b213b5dfac97ba860d5a203268d5f9733b2
CamilleZxt <1374388481@qq.com>
2026-01-13 13:58:07 +0800

    test ed25519

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,8 +1,12 @@
 use serde::{Deserialize, Serialize};
+use serde_big_array::BigArray;
+
 
 #[derive(Serialize, Deserialize)]
 pub struct Ed25519VerificationData {
     pub pub_key: [u8; 32],      // Ed25519 公钥 (32字节)
+    #[serde(with = "BigArray")]
     pub signature: [u8; 64],    // Ed25519 签名 (64字节)
+
     pub message: Vec<u8>,       // 交易内容 X
 }
